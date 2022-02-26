c7dbe6177a5d99ffbbfbaeb3652e3466145782d2
Alexander Sánchez <71980510+alexSanchez0516@users.noreply.github.com>
2022-02-26 05:23:58 +0100

    Update db.php

--- includes/config/db.php
+++ includes/config/db.php
@@ -3,7 +3,7 @@
 
 
 function connectDB() : mysqli{
-    $db = new mysqli('localhost', 'xymind', '1690001299Gr.', 'divisione_crud');
+    //$db = new mysqli('localhost', 'xymind', '', 'divisione_crud'); pass:169
     if (!$db) {
         echo "No hay conexion";
         exit;
